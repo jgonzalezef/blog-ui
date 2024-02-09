d5cdbf7b83b8c6ed4a2ac8254d0509b6c14032ce
jgonzalezef <jesusgonzalez@MacBook-Pro-de-Envia.local>
2024-02-09 13:53:28 -0600

    components

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { GlobalField } from './components/forms/globalField'
+import { Paragraph } from './components/globals/Paragraph'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='form-content'>
+
+        <Paragraph text="Ùnete a nuestra comunidad estudiantil" />
+
+        <Paragraph 
+          text="Bienvenido a nuestra red social estudiantil. Registrate ahora para conectarte con compañeros, acceder a recursos educativos y participar en eventos emocionantes"  
+        />
+
+        <GlobalField label="Nombre(s)" placeholder="Nombre(s)" />
+        <GlobalField label="Apellido(s)" placeholder="Nombre(s)" />
+        <GlobalField label="Correo Electronico" placeholder="Correo Electronico" type="email" />
+        <GlobalField label="Contraseña" placeholder="******" type="password" />
+        <GlobalField label="Confirmar Contraseña" placeholder="****" type="password" />
+        
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
