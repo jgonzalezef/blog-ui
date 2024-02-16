a9783fdc554aaed1b9a9987ed72c7983ab736d27
jgonzalezef <jesusgonzalez@MacBook-Pro-de-Envia.local>
2024-02-16 13:12:37 -0600

    componente de entrada

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,33 @@
 import './App.css'
 import { GlobalField } from './components/forms/globalField'
 import { Paragraph } from './components/globals/Paragraph'
+import { Post } from './components/globals/Post'
 
 function App() {
 
 
   return (
     <>
-      <div className='form-content'>
+      <div className="posts">
+        <Post/>
 
-        <Paragraph text="Ùnete a nuestra comunidad estudiantil" />
+        <Post/>
 
-        <Paragraph 
-          text="Bienvenido a nuestra red social estudiantil. Registrate ahora para conectarte con compañeros, acceder a recursos educativos y participar en eventos emocionantes"  
-        />
+        <Post/>
 
-        <GlobalField label="Nombre(s)" placeholder="Nombre(s)" />
-        <GlobalField label="Apellido(s)" placeholder="Nombre(s)" />
-        <GlobalField label="Correo Electronico" placeholder="Correo Electronico" type="email" />
-        <GlobalField label="Contraseña" placeholder="******" type="password" />
-        <GlobalField label="Confirmar Contraseña" placeholder="****" type="password" />
-        
+        <Post/>
+
+        <Post/>
+
+        <Post/>
+
+        <Post/>
+
+        <Post/>
+
+        <Post/>
       </div>
+      
     </>
   )
 }
