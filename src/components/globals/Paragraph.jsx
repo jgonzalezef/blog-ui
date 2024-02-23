af760059aaee8a6686c4ab149f2af5f0287da303
jgonzalezef <jesusgonzalez@MacBook-Pro-de-Envia.local>
2024-02-22 23:53:01 -0600

    organizacion de componentes refactorizacion

--- src/components/globals/Paragraph.jsx
+++
@@ -1,9 +0,0 @@
-export function Paragraph({text,size='16px',color='black',orientation='center'}) {
-    return (
-        <>
-            <p style={{'color':color,'font-size':size,'text-align': orientation}}>
-                {text}
-            </p>
-        </>
-    );
-}
